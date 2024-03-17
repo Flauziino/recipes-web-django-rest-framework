fda0e51910955cbb0e1d34381104436820568ee5
Lucas Flauzino <27flauzino@gmail.com>
2024-03-17 09:02:21 -0300

    continuando os testes da api list

--- recipes/tests/test_recipe_api_list.py
+++ recipes/tests/test_recipe_api_list.py
@@ -71,3 +71,55 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
 
             # qtd pg1
             self.assertEqual(qtd_recipe_pg_10, 2)
+
+    def test_recipes_api_do_not_loads_recipes_if_is_published_false(self):  # noqa: E501
+        # criando a receita para o teste
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipes-api-list'
+            )
+        )
+
+        # checando se ela existe
+        self.assertEqual(
+            response.data.get('count'), 0
+        )
+
+    def test_recipe_api_list_loads_recipes_by_category_id(self):
+        # criando duas categorias (desejada e nao desejada)
+        category_wanted = self.make_category(name='WANTED CATEGORY')
+        category_not_wanted = self.make_category(name='NOT_WANTED CATEGORY')
+
+        # criando 10 receitas
+        recipes = self.make_recipe_in_batch(qtd=10)
+
+        # fazendo todas receitas ter a categoria desejada
+        for recipe in recipes:
+            recipe.category = category_wanted
+            recipe.save()
+
+        # mudando a categoria de apenas uma receita para a nao desejada
+        recipes[0].category = category_not_wanted
+        recipes[0].save()
+
+        api_url = reverse(
+            'recipes:recipes-api-list'
+        ) + f'?category_id={category_wanted.id}'
+        response = self.client.get(api_url)
+
+        # checando se tem a quantidade desejada
+        # das categorias que estamos buscando
+        self.assertEqual(
+            response.data.get('count'), 9
+        )
+
+        # conferindo se o mesmo ocorre para a outra categoria
+        api_url = reverse(
+            'recipes:recipes-api-list'
+        ) + f'?category_id={category_not_wanted.id}'
+        response = self.client.get(api_url)
+        self.assertEqual(
+            response.data.get('count'), 1
+        )
